970c0d92af4cd6f5e82f1f2b9a84e62fbc892fb7
Elias Soares <esoares@iget.com.br>
2014-12-18 10:27:01 -0300

    Adicionado Fillable no users

--- app/models/User.php
+++ app/models/User.php
@@ -23,4 +23,6 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+	protected $fillable = array('name', 'surname', 'email', 'password', 'level');
+
 }
