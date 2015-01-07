0e3662847f1eb7cb211b2c947f417adf7f5d0add
Elias Soares <esoares@iget.com.br>
2015-01-07 14:48:30 -0300

    Melhorias no formulario de criação de usuário

--- app/views/user/create.blade.php
+++ app/views/user/create.blade.php
@@ -72,149 +72,149 @@
 		</div>
 		</div>
 			<div id="user-times" class="col-md-5">
-				<div class="header">
-					<div class="col-lg-4"><span class="day">Dia</span></div>
-					<div class="col-lg-3"><span class="time_in" style="margin-left:-5px;">Entrada</span></div>
-					<div class="col-lg-3"><span class="time_out" style="margin-left:4px;">Saída</span></div>
+				<div class="row header">
+					<div class="col-xs-4"><span class="day">Dia</span></div>
+					<div class="col-xs-3"><span class="time_in">Entrada</span></div>
+					<div class="col-xs-3"><span class="time_out">Saída</span></div>
 				</div>
 
 
 				<div class="day row">
-					<div class="col-lg-4">
+					<div class="col-xs-4">
 						<div class="checkbox"><label>{{Form::checkbox('day_check_0', '1') }} Domingo</label></div>
 					</div>
 					
 					@if ($errors->has('day_0_time_in'))
 						{{ $errors->first('day_0_time_in') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_0_time_in', (isset($day_0_time_in) ? substr($day_0_time_in,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div> 
 
 					@if ($errors->has('day_0_time_out'))
 						{{ $errors->first('day_0_time_out') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_0_time_out', (isset($day_0_time_out) ? substr($day_0_time_out,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div>
 				</div>
 
 				<div class="day row">
-					<div class="col-lg-4">
+					<div class="col-xs-4">
 						<div class="checkbox"><label>{{Form::checkbox('day_check_1', '1') }} Segunda-feira</label></div>
 					</div>
 					
 					@if ($errors->has('day_1_time_in'))
 						{{ $errors->first('day_1_time_in') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_1_time_in', (isset($day_1_time_in) ? substr($day_1_time_in,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div> 
 
 					@if ($errors->has('day_1_time_out'))
 						{{ $errors->first('day_1_time_out') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_1_time_out', (isset($day_1_time_out) ? substr($day_1_time_out,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div>
 				</div>
 
 				<div class="day row">
-					<div class="col-lg-4">
+					<div class="col-xs-4">
 						<div class="checkbox"><label>{{Form::checkbox('day_check_2', '1') }} Terça-feira</label></div>
 					</div>
 					
 					@if ($errors->has('day_2_time_in'))
 						{{ $errors->first('day_2_time_in') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_2_time_in', (isset($day_2_time_in) ? substr($day_2_time_in,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div> 
 
 					@if ($errors->has('day_2_time_out'))
 						{{ $errors->first('day_2_time_out') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_2_time_out', (isset($day_2_time_out) ? substr($day_2_time_out,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div>
 				</div>
 
 				<div class="day row">
-					<div class="col-lg-4">
+					<div class="col-xs-4">
 						<div class="checkbox"><label>{{Form::checkbox('day_check_3', '1') }} Quarta-feira</label></div>
 					</div>
 					
 					@if ($errors->has('day_3_time_in'))
 						{{ $errors->first('day_3_time_in') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_3_time_in', (isset($day_3_time_in) ? substr($day_3_time_in,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div> 
 
 					@if ($errors->has('day_3_time_out'))
 						{{ $errors->first('day_3_time_out') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_3_time_out', (isset($day_3_time_out) ? substr($day_3_time_out,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div>
 				</div>
 
 				<div class="day row">
-					<div class="col-lg-4">
+					<div class="col-xs-4">
 						<div class="checkbox"><label>{{Form::checkbox('day_check_4', '1') }} Quinta-feira</label></div>
 					</div>
 					
 					@if ($errors->has('day_4_time_in'))
 						{{ $errors->first('day_4_time_in') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_4_time_in', (isset($day_4_time_in) ? substr($day_4_time_in,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div> 
 
 					@if ($errors->has('day_4_time_out'))
 						{{ $errors->first('day_4_time_out') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_4_time_out', (isset($day_4_time_out) ? substr($day_4_time_out,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div>
 				</div>
 
 				<div class="day row">
-					<div class="col-lg-4">
+					<div class="col-xs-4">
 						<div class="checkbox"><label>{{Form::checkbox('day_check_5', '1') }} Sexta-feira</label></div>
 					</div>
 					
 					@if ($errors->has('day_5_time_in'))
 						{{ $errors->first('day_5_time_in') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_5_time_in', (isset($day_5_time_in) ? substr($day_5_time_in,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div> 
 
 					@if ($errors->has('day_5_time_out'))
 						{{ $errors->first('day_5_time_out') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_5_time_out', (isset($day_5_time_out) ? substr($day_5_time_out,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div>
 				</div>
 
 				<div class="day row">
-					<div class="col-lg-4">
+					<div class="col-xs-4">
 						<div class="checkbox"><label>{{Form::checkbox('day_check_6', '1') }} Sábado</label></div>
 					</div>
 					
 					@if ($errors->has('day_6_time_in'))
 						{{ $errors->first('day_6_time_in') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_6_time_in', (isset($day_6_time_in) ? substr($day_6_time_in,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div> 
 
 					@if ($errors->has('day_6_time_out'))
 						{{ $errors->first('day_6_time_out') }}
 					@else
-						<div class="col-lg-3">
+						<div class="col-xs-3">
 					@endif
 						{{Form::text('day_6_time_out', (isset($day_6_time_out) ? substr($day_6_time_out,0,-3) : null), array('class' => 'form-control', 'disabled'=>'true'))}}</div>
 				</div>
