627fdfe4655e247e238981ebd9fdda187c9a615e
Elias Soares <esoares@iget.com.br>
2015-01-07 15:00:12 -0300

    Criada Foreign Key para user_id na timetable

---
+++ app/database/migrations/2015_01_07_175920_create_timetables_foreign.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimetablesForeign extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('timetable', function(Blueprint $table)
+		{
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('timetable', function(Blueprint $table)
+		{
+			$table->dropForeign('timetables_user_id_foreign');
+		});
+	}
+
+}
