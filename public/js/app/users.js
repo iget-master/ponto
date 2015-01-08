5763e4687dc5adfec39cc588016976994b989872
Elias Soares <esoares@iget.com.br>
2015-01-08 15:53:56 -0300

    Finalizada exclusão multipla de usuários
    * Finalizada exclusão múltipla de usuários
    * Melhorado layout dos Alerts
    * Modificado database seeder para excluir dados anteriores.
    
    Fixes #21

--- public/js/app/users.js
+++ public/js/app/users.js
@@ -12,4 +12,11 @@
 		})
 	});
 
+	$("#users_table").on('selection-change', function(event) {
+		$('#delete_users input[name="id"]').remove();
+		$("#users_table tbody").find('input[type="checkbox"]:checked').each(function() {
+			$("#delete_users").append('<input type="hidden" name="id[]" value="' + $(this).closest('tr').data('id') + '">');
+		});
+	})
+
 } (jQuery)
