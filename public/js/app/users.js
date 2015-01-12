22fed7b6924f693e2d351fbd45a9131522ac963b
Elias Soares <esoares@iget.com.br>
2015-01-12 10:09:33 -0300

    Corrige bug no #21

--- public/js/app/users.js
+++ public/js/app/users.js
@@ -13,7 +13,7 @@
 	});
 
 	$("#users_table").on('selection-change', function(event) {
-		$('#delete_users input[name="id"]').remove();
+		$('#delete_users input[name="id[]"]').remove();
 		$("#users_table tbody").find('input[type="checkbox"]:checked').each(function() {
 			$("#delete_users").append('<input type="hidden" name="id[]" value="' + $(this).closest('tr').data('id') + '">');
 		});
