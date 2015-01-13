b2f245749fcb835fea094c7ef5d36c79c62639eb
iget-jmoreira <jmoreira@iget.com.br>
2015-01-13 09:21:56 -0300

    Criado relogio de ponto analógico
    Sem um layout definido

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -27,7 +27,7 @@
 					@endfor
 				</div>
 			</div>
-			<div class="col-md-9">
+			<div class="col-md-4">
 				<div class="block-row clearfix">
 					<div class="block">
 						<p class="title">{{ $statistics["dias_trabalhados"] }}</p>
@@ -59,7 +59,184 @@
 					</div> -->
 				</div>
 			</div>
+			<div class="col-md-4" align="center">
+				<canvas id="example" width="300px" height="300px">Testando o relogio</canvas>
+				<?php 
+					if(Timetables::getToday()){
+						if($timeIn = Timetables::getToday()->time_in){
+							$timeIn_h = substr($timeIn, 0, -6);
+							$timeIn_m = substr($timeIn, -5,-3);
+							$timeIn_s = substr($timeIn, -2);
+						} else{
+							$timeIn_h = "hrs";
+							$timeIn_m = "min";
+							$timeIn_s = "sec";
+						}
+						if($timeOut = Timetables::getToday()->time_out){
+							$timeOut_h = substr($timeOut, 0, -6);
+							$timeOut_m = substr($timeOut, -5, -3);
+							$timeOut_s = substr($timeOut, -2);
+						} else{
+							$timeOut_h = "hrs";
+							$timeOut_m = "min";
+							$timeOut_s = "sec";
+						}
+					} else{
+						$timeIn_h = "hrs";
+						$timeIn_m = "min";
+						$timeIn_s = "sec";
+
+						$timeOut_h = "hrs";
+						$timeOut_m = "min";
+						$timeOut_s = "sec";
+					}
+
+				?>
+				    	
+				<script type="application/x-javascript">
+					draw();
+				    function draw() {
+
+				      	var canvas = document.getElementById('example');
+				        var c2d=canvas.getContext('2d');
+						c2d.moveTo(150,150);
+						c2d.arc(150,150,120,0,90);
+						c2d.lineTo(150,150);
+						c2d.stroke();
+
+						c2d.clearRect(0,0,300,300);
+				        //Define gradients for 3D / shadow effect
+				        var grad1=c2d.createLinearGradient(0,0,300,300);
+				        grad1.addColorStop(0,"#D83040");
+				        grad1.addColorStop(1,"#801020");
+				        var grad2=c2d.createLinearGradient(0,0,300,300);
+				        grad2.addColorStop(0,"#801020");
+				        grad2.addColorStop(1,"#D83040");
+				        c2d.font="Bold 20px Arial";
+				        c2d.textBaseline="middle";
+				        c2d.textAlign="center";
+				        c2d.lineWidth=1;
+				        c2d.save();
+				        //Outer bezel
+				        c2d.strokeStyle=grad1;
+				        c2d.lineWidth=10;
+				        c2d.beginPath();
+				        c2d.arc(150,150,138,0,Math.PI*2,true);
+				        c2d.shadowOffsetX=4;
+				        c2d.shadowOffsetY=4;
+				        c2d.shadowColor="rgba(0,0,0,0.6)";
+				        c2d.shadowBlur=6;
+				        c2d.stroke();
+				        //Inner bezel
+				        c2d.restore();
+				        c2d.strokeStyle=grad2;
+				        c2d.lineWidth=10;
+				        c2d.beginPath();
+				        c2d.arc(150,150,129,0,Math.PI*2,true);
+				        c2d.stroke();
+				        c2d.strokeStyle="#222";
+				        c2d.save();
+				        c2d.translate(150,150);
+				        //Markings/Numerals
+				        for (i=1;i<=60;i++) {
+				          ang=Math.PI/30*i;
+				          sang=Math.sin(ang);
+				          cang=Math.cos(ang);
+				          //If modulus of divide by 5 is zero then draw an hour marker/numeral
+				          if (i % 5 == 0) {
+				            c2d.lineWidth=8;
+				            sx=sang*95;
+				            sy=cang*-95;
+				            ex=sang*120;
+				            ey=cang*-120;
+				            nx=sang*80;
+				            ny=cang*-80;
+				            //writes the numbers
+				            c2d.fillStyle = "rgb(0, 0, 0)";
+				            c2d.fillText(i/5,nx,ny);
+				          //Else this is a minute marker
+				          } else {
+				            c2d.lineWidth=2;
+				            sx=sang*110;
+				            sy=cang*110;
+				            ex=sang*120;
+				            ey=cang*120;
+				          }
+				          c2d.beginPath();
+				          c2d.moveTo(sx,sy);
+				          c2d.lineTo(ex,ey);
+				          c2d.stroke();
+				        }
+				        //Fetch the current time
+				        var now=new Date();
+				        var hrs=now.getHours();
+				        var min=now.getMinutes();
+				        var sec=now.getSeconds();
+
+				        var timeIn_s = {{$timeIn_s}};
+					    var timeIn_m = {{$timeIn_m}};
+					    var timeIn_h = {{$timeIn_h}};
+					
+				        var timeOut_s = {{$timeOut_s}};
+					    var timeOut_m = {{$timeOut_m}};
+					    var timeOut_h = {{$timeOut_h}};
+						
+				        
+				        c2d.strokeStyle="#000";
+				        
+				        c2d.lineWidth=6;
+				        c2d.save();
+				        //Draw clock pointers but this time rotate the canvas rather than
+				        //calculate x/y start/end positions.
+				        //
+				        //Draw hour hand
+				        c2d.rotate(Math.PI/6*(hrs+(min/60)+(sec/3600)));
+				        c2d.beginPath();
+				        c2d.moveTo(0,10);
+				        c2d.lineTo(0,-60);
+				        c2d.stroke();
+				        c2d.restore();
+				        c2d.save();
+				        //Draw minute hand
+				        c2d.rotate(Math.PI/30*(min+(sec/60)));
+				        c2d.beginPath();
+				        c2d.moveTo(0,20);
+				        c2d.lineTo(0,-110);
+				        c2d.stroke();
+				        c2d.restore();
+				        c2d.save();
+				        //Draw second hand
+		        		c2d.rotate(Math.PI/30*sec);
+		        		c2d.strokeStyle="#E33";
+		        		c2d.beginPath();
+		        		c2d.moveTo(0,20);
+		        		c2d.lineTo(0,-110);
+		        		c2d.stroke();
+		        		c2d.restore();
+
+				      	
+				        
+				        //Additional restore to go back to state before translate
+				        //Alternative would be to simply reverse the original translate
+				        c2d.restore();
+				        setTimeout(draw,1000);
+
+				        var context=canvas.getContext('2d');
+				        context.moveTo(150,150);
+				        context.lineTo(150,150);
+				        context.arc(150,150,135,(Math.PI/6*(timeIn_h+(timeIn_m/60)+(timeIn_s/3600))) - 1.57, (Math.PI/6*(timeOut_h+(timeOut_m/60)+(timeOut_s/3600))) - 1.57);
+				        context.lineWidth = 5;
+				        context.strokeStyle = "rgba(0,0,200,0.2)";
+				        // context.strokeStyle = '#ff0000';
+				        context.stroke();
+				        context.fillStyle = "rgba(0, 0, 200, 0.5)";
+				        context.fill();
+
+				  	}	   
+				</script>
+			</div>
 		</div>
+
 	</div>
 @stop
 
