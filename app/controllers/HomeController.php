09f8be55dfe67d2a12e8bc683b93adb1de720d47
iget-jmoreira <jmoreira@iget.com.br>
2015-01-22 11:29:26 -0300

    Melhorias na estatistica de Horas Trabalhadas

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -51,18 +51,14 @@ class HomeController extends BaseController {
 		$faltas = 0;
 		$atrasos = 0;
 		$horasTrabalhadas = 0;
+		$worked_hours = 0;
+		$worked_minutes = 0;
+		$worked_mins = 0;
 
 		for ($i=0; $i < intval(date('t', $timestamp)); $i++) {
 			$day = strtotime("+${i} day", $firstDayOfMonth);
 			$day_times = Timetables::getDay($day);
 			$class = "";
-
-			if($day_times){
-				if($day_times->time_out != null){
-					$worked = substr($day_times->time_out,0,-6) - substr($day_times->time_in,0,-6);
-					$horasTrabalhadas += $worked;
-				}
-			}
 
 			$pontual = true;
 			$falta = false;
@@ -97,8 +93,22 @@ class HomeController extends BaseController {
 			$calendar["days"][] = ["date"=>$day, "class"=>$class, "timetable"=>$day_times];
 			$calendarDayCount++;
 
+			if($day_times){
+				if($day_times->time_out != null){
+					$hours = substr($day_times->time_out,0,-6) - substr($day_times->time_in,0,-6);
+					$minutes = substr($day_times->time_out,3,-3) - substr($day_times->time_in,3,-3);
+					$worked_hours += $hours;
+					$worked_minutes += $minutes;
+					while($worked_minutes >= 60){
+						$worked_minutes -= 60;
+						$worked_mins++;
+					}
+				}
+			}
 
 		}
+
+		echo $horasTrabalhadas += $worked_hours;
 
 		// Varre dias após o mês atual
 		for ($i=1; $i <= (42 - $calendarDayCount); $i++) {
@@ -111,7 +121,7 @@ class HomeController extends BaseController {
 		$statistics["dias_trabalhados"] = $dias - $faltas;
 		$statistics["faltas"] = $faltas;
 		$statistics["atrasos"] = $atrasos;
-		$statistics["horasTrabalhadas"] = $horasTrabalhadas;
+		$statistics["horasTrabalhadas"] = $horasTrabalhadas + $worked_mins;
 		if ($statistics["dias_trabalhados"] == 0) {
 			$statistics["pontualidade"] = "-";
 		} else {
